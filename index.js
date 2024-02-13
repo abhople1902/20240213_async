3044ddfcd5278df21b1558afa2c9e8a0b40aee71
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-02-13 17:34:17 +0530

    trying to print numbers every second

--- index.js
+++ index.js
@@ -42,13 +42,75 @@
 
 
 
-const express = require('express')
-const app = express()
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+
+// let counter = 0
+// let upCount = 10
+
+// function countDown() {
+//   res.send(counter)
+//   upCount--
+// }
+
+// app.get('/', function (req, res) {
+
+//   setInterval(countDown, 1000)
+
+//   const countDown = () => {
+//     res.send(counter)
+//     upCount--
+//   }
+
+// })
+
+// app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const app = express();
+
+let counter = 0;
+let upCount = 10;
+
+function countDown(res) {
+  res.send(counter.toString());
+  counter++;
+  upCount--;
+
+  if (upCount === 0) {
+    clearInterval(intervalId);
+  }
+}
 
 app.get('/', function (req, res) {
+  const intervalId = setInterval(() => countDown(res), 1000);
 
-  setTimeout(() => res.send('Hello Innovapptive'), 1500)
-  
-})
+  res.send(counter.toString());
 
-app.listen(3000)
+  req.on('close', () => {
+    clearInterval(intervalId);
+  });
+});
+
+app.listen(3000);
